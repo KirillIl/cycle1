3fbce220ca533f84821f75060ba9d18dca229369
Admin <ilinskiy_kirill@mail.ru>
2022-10-03 16:51:54 +0300

    home work 2

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Задание 1");
+        for (int i = 1904; i <= 2096; i+=4) {
+            System.out.println( i + " год является високосным");
+        }
+        System.out.println( "Задание 2");
+        for (int i = 7; i <= 98 ; i+=7) {
+            System.out.println(i);
+            
+        }
+        System.out.println("Задание 3");
+        for (int i = 1; i <= 512; i= i * 2) {
+            System.out.println(i);
+            
+        }
+
+    }
+}
